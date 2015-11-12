b081f71999d2a843bdb1b8922585337c36f5fe9f
graffjosh <Github2@joshuagraff.com>
2015-11-12 16:58:31 -0500

    Final

---
+++ test/TestClass.java
@@ -0,0 +1,63 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import converter;
+public class TestClass {
+
+
+	@Test
+	public void correctInputRoman() throws MalformedNumberException,ValueOutOfBoundsException {
+		try{
+		//test valid input to the roman to arabic converter
+		assertEquals(RomanArabicConverter.RomanArabicConverter("  MMM  "), "3000");
+		assertEquals(RomanArabicConverter.RomanArabicConverter("ix"),"9");
+		assertEquals(RomanArabicConverter.RomanArabicConverter("xi"),"11");
+		assertEquals(RomanArabicConverter.RomanArabicConverter("mcm"),"1900");
+		assertEquals(RomanArabicConverter.RomanArabicConverter("xlv"),"45");
+		assertEquals(RomanArabicConverter.RomanArabicConverter(""),"0");
+		
+		}catch(MalformedNumberException e)
+		{
+			System.out.println(e);
+		}
+
+	}
+	
+	@Test
+	public void validInputArabic() throws MalformedNumberException, ValueOutOfBoundsException {
+		try{
+			assertEquals(RomanArabicConverter.RomanArabicConverter("3000"), "MMM");
+			assertEquals(RomanArabicConverter.RomanArabicConverter("9"),"IX");
+			assertEquals(RomanArabicConverter.RomanArabicConverter("11"),"XI");
+			assertEquals(RomanArabicConverter.RomanArabicConverter("1900"),"MCM");
+			assertEquals(RomanArabicConverter.RomanArabicConverter("45"),"XLV");
+			assertEquals(RomanArabicConverter.RomanArabicConverter("5"),"V");
+			assertEquals(RomanArabicConverter.RomanArabicConverter("0"),"");
+		}
+		catch(ValueOutOfBoundsException e)
+		{
+			System.out.println(e);
+		}
+	}
+/*	
+	@Test
+	public void invalidInputRoman() throws MalformedNumberException {
+		try{
+			System.out.println(RomanArabicConverter.toArabic("MM m"));
+			System.out.println(RomanArabicConverter.toArabic("iix"));
+			System.out.println(RomanArabicConverter.toArabic("xM"));
+			System.out.println(RomanArabicConverter.toArabic("Z q t"));
+			System.out.println(RomanArabicConverter.toArabic("113"));
+			System.out.println(RomanArabicConverter.toArabic("MMMM"));
+		}
+		catch(MalformedNumberException e)
+		{
+			System.out.println(e);
+		}
+	}*/
+
+}
